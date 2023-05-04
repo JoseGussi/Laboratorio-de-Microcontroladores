d33f2b6a4ea9fc42041e9c18145e825cf0043f66
Jose Mario Gonzalez <56488359+jomagona01@users.noreply.github.com>
2023-05-03 22:33:41 -0600

    Añadiendo modo AC
    El sistema ya está diseñado para medir voltajes AC. Con el switch cerrado se mide DC y abierto AC. Nota: Es necesario desconectar la fuenta AC y si se está midiendo DC y viceversa, para no generar interferencia en las mediciones.

--- Lab03_ProgressTracking/src/Final/voltimetro.ino
+++ Lab03_ProgressTracking/src/Final/voltimetro.ino
@@ -2,10 +2,21 @@
 #include <PCD8544.h>
 
 const int analogPin = A0;
+const int PinSwitch = A5;
 const float Vref = 5.0;
-const int ADCResolution = 1023;
-const float R1 = 1000.0;
-const float R2 = 10000.0;
+const float ADCResolution = 1023;
+const int pinBoton = 5;
+bool estadoBoton;
+
+
+
+
+const int numMuestras = 100; // Número de muestras que deseas tomar
+const int pinSenal = A0; // Pin analógico al que está conectada la señal
+const int intervaloMuestreo = 1; // Intervalo de tiempo entre muestras en milisegundos
+float muestras[numMuestras]; // Vector para almacenar las muestras
+
+
 
 // Define los pines para conectar la pantalla LCD
 #define SCLK 3
@@ -25,16 +36,53 @@ void setup() {
 
   // Add the smiley to position "0" of the ASCII table...
   lcd.createChar(0, glyph);
-  
+  pinMode(pinBoton, INPUT_PULLUP);
 }
 
+
+void tomarMuestras() {
+  for (int i = 0; i < numMuestras; i++) {
+    muestras[i] = analogRead(analogPin);
+    delay(intervaloMuestreo);
+  }
+}
+
+float calcularVRMS() {
+  float sumaCuadrados = 0;
+  for (int i = 0; i < numMuestras; i++) {
+    //float voltaje = (muestras[i] / 1023.0) * 5.0; // Convierte la muestra a voltaje (asumiendo una referencia de 5V)
+    float voltajeA = ((511.0 - muestras[i])) * 56.0/1023.0;
+    sumaCuadrados += pow(voltajeA, 2); // Eleva al cuadrado y suma al acumulador
+  }
+  float promedioCuadrados = sumaCuadrados / numMuestras; // Calcula el promedio de los cuadrados
+  float vRMS = sqrt(promedioCuadrados); // Calcula la raíz cuadrada del promedio
+  return vRMS;
+}
+
+
 void loop() {
-  int adcValue = analogRead(analogPin); //Reads analog input
-  float voltage = ((511.0 - adcValue)) * 49.0/1023.0; //Substracts the 2.5V offset, to start in 0, then retrieves the real value
+  bool estadoSwitch = digitalRead(PinSwitch);
 
-  lcd.setCursor(0, 0);
+  estadoBoton = digitalRead(pinBoton);
+  if(estadoSwitch == 1){
+    int adcValue1 = analogRead(analogPin); //Reads analog input
+    float voltage = ((511.0 - adcValue1)) * 49.0/1023.0; //Substracts the 2.5V offset, to start in 0, then retrieves the real value
+    lcd.setCursor(0, 0);
+    lcd.print("V1: ");
+    lcd.print(voltage);
+    lcd.print("V");
+  }
+  else{
+    tomarMuestras(); // Toma las muestras de la señal sinusoidal
+    float vRMS = calcularVRMS(); // Calcula el valor RMS
+    lcd.setCursor(0, 0);
+    lcd.print("V1: ");
+    lcd.print(vRMS);
+    lcd.print("V");
+  }
+  
 
-  lcd.print("V 1: ");
-  lcd.print(voltage);
-  //lcd.print("V");
+
+
+  delay(1000); // Espera un segundo antes de tomar más muestras
 }
