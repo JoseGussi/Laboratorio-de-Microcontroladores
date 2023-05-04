a642149931a089eb1c6a960d4ae885e2a0188f15
Jose Gonzalez <50153070+JoseGussi@users.noreply.github.com>
2023-05-04 00:46:47 -0600

    Script de comunicacion
    Script de python para la comunicacion serial de con los puertos

---
+++ Lab03_ProgressTracking/src/Final/comunication.py
@@ -0,0 +1,12 @@
+import serial, csv
+
+lectura_serial = serial.Serial("/tmp/ttyS0", 9600) #Comunicacion con la placa mediante el puerto ttyS0
+
+filename= open("Voltajes.csv",'w') 
+escritura_archivo = csv.writer(filename)
+
+while (True):
+	data = lectura_serial.readline().decode().split(' ')
+	escritura_archivo.writerow(data)
+
+filename.close()
