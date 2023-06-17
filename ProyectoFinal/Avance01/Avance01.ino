11152bd75feeaa1a9c4e0af2e5037b8e51e43513
Jose Carlos Gonzalez Cordero <josini2000@gmail.com>
2023-06-17 15:02:52 -0600

    Agregando avances servo y bot telegram

---
+++ ProyectoFinal/Avance01/Avance01.ino
@@ -0,0 +1,78 @@
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <ESP32Servo.h>
+#include <UniversalTelegramBot.h>
+#include <ArduinoJson.h>
+
+Servo myservo;  // crea el objeto servo
+
+// GPIO de salida del servo
+static const int servoPin = 13;
+
+// Reemplazar con tus credenciales de Wifi
+const char* ssid     = "QQice02";
+const char* password = "monica321";
+
+// Inicializa Bot Telegram
+#define BOTtoken "6203517484:AAE9qgnCQEj7QHX56B6H3dnD7fysqqfhGZo"  // Tu Bot Token (Obtener de Botfather)
+
+#define CHAT_ID "1076728481"
+
+WiFiClientSecure client;
+
+UniversalTelegramBot bot(BOTtoken, client);
+
+void handleNewMessages(int numNewMessages) {
+
+  for (int i=0; i<numNewMessages; i++) {
+    // Chat id of the requester
+    String chat_id = String(bot.messages[i].chat_id);
+    if (chat_id != CHAT_ID){
+      bot.sendMessage(chat_id, "Usuario no autorizado", "");
+      continue;
+    }
+
+    String text = bot.messages[i].text;
+
+    if (text == "/comida") {
+      bot.sendMessage(chat_id, "Alimentando", "");
+      myservo.write(80);             
+      delay(500);     
+      myservo.write(0); 
+      
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  myservo.attach(servoPin);  // vincula el servo en el servoPin
+
+  // Conecta a red WiFi con SSID y password
+  Serial.print("Conectado a ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  client.setCACert(TELEGRAM_CERTIFICATE_ROOT);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  // Muestra IP local 
+  Serial.println("");
+  Serial.println("WiFi conectado.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  bot.sendMessage(CHAT_ID, "Bot iniciado", "");
+}
+
+void loop() {
+  int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+
+  while(numNewMessages) {
+    handleNewMessages(numNewMessages);
+    numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+  }
+}
